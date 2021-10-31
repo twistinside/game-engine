c39d8225b2604a509c405fcba3be2f05e1772be3
twistinside <twistinside@users.noreply.github.com>
2021-10-31 10:58:57 -0700

    Create project

---
+++ Game Engine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Game Engine
+//
+//  Created by Karl Groff on 10/31/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
