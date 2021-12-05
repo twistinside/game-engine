5f98ea4bfa67626f39395d3222a01152fb2d3845
twistinside <twistinside@users.noreply.github.com>
2021-12-05 10:42:36 -0800

    Metal part 5: The MTL Vertex Descriptor

--- Game Engine/GameView.swift
+++ Game Engine/GameView.swift
@@ -1,11 +1,6 @@
 import MetalKit
 
 class GameView: MTKView {
-
-    struct Vertex {
-        var position: float3
-        var color: float4
-    }
     
     var commandQueue: MTLCommandQueue!
     var renderPipelineState: MTLRenderPipelineState!
@@ -34,18 +29,33 @@ class GameView: MTKView {
     }
         
     func createBuffers() {
-        vertexBuffer = device?.makeBuffer(bytes: vertices, length: MemoryLayout<Vertex>.stride * vertices.count, options: [])
+        vertexBuffer = device?.makeBuffer(bytes: vertices, length: Vertex.stride(vertices.count) * vertices.count, options: [])
     }
     
     func createRenderPipelineState() {
         let library = device?.makeDefaultLibrary()
         let vertexFunction = library?.makeFunction(name: "basic_vertex_shader")
         let fragmentFunction = library?.makeFunction(name: "basic_fragment_shader")
+        
+        let vertexDescriptor = MTLVertexDescriptor()
+        
+        // position
+        vertexDescriptor.attributes[0].format = .float3
+        vertexDescriptor.attributes[0].bufferIndex = 0
+        vertexDescriptor.attributes[0].offset = 0
+        
+        // color
+        vertexDescriptor.attributes[1].format = .float4
+        vertexDescriptor.attributes[1].bufferIndex = 0
+        vertexDescriptor.attributes[1].offset = float3.stride()
+        
+        vertexDescriptor.layouts[0].stride = Vertex.stride()
 
         let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
         renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
         renderPipelineDescriptor.vertexFunction = vertexFunction
         renderPipelineDescriptor.fragmentFunction = fragmentFunction
+        renderPipelineDescriptor.vertexDescriptor = vertexDescriptor
 
         do {
             renderPipelineState = try device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
--- Game Engine/MyShaders.metal
+++ Game Engine/MyShaders.metal
@@ -2,8 +2,8 @@
 using namespace metal;
 
 struct VertexIn {
-    float3 position;
-    float4 color;
+    float3 position [[ attribute(0) ]];
+    float4 color [[ attribute(1) ]];
 };
 
 struct RasterizerData {
@@ -11,11 +11,10 @@ struct RasterizerData {
     float4 color;
 };
 
-vertex RasterizerData basic_vertex_shader(device VertexIn *vertices [[ buffer(0) ]],
-                                          uint vertexID [[ vertex_id ]]) {
+vertex RasterizerData basic_vertex_shader(const VertexIn vIn [[ stage_in ]]) {
     RasterizerData rd;
-    rd.position = float4(vertices[vertexID].position, 1);
-    rd.color    = vertices[vertexID].color;
+    rd.position = float4(vIn.position, 1);
+    rd.color    = vIn.color;
     return rd;
 }
 
